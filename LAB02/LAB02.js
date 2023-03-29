2541968c8abf19c5c040f5f530c55b1aa79b2486
danieliusre <danielius.rekus@gmail.com>
2023-03-29 13:07:31 +0100

    LAB02 files + task1

---
+++ LAB02/LAB02.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#rotate").cycle({ fx: "scrollHorz" });
+});
+
+/* FOR HIDING DIV */
+$(document).ready(function () {
+    $("#btn2").click(function () {
+        $("#div2").toggle(2000);
+    });
+});
